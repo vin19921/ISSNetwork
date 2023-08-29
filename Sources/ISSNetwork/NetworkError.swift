bbc350551a5e7aca83365d08b66613cc7a796faf
Wing Seng <chewwingseng@gmail.com>
2023-08-29 14:56:06 +0800

    update

---
+++ Sources/ISSNetwork/NetworkError.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Wing Seng Chew on 29/08/2023.
+//
+
+import Foundation
+
+public enum NetworkError: String, Error {
+    case internetFailure = "INTERNET_FAILURE"
+}
