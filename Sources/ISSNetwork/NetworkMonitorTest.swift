047da5bab505a126b7099face5f8a732514bb96e
Wing Seng <chewwingseng@gmail.com>
2023-09-05 14:18:39 +0800

    Update NetworkMonitorTest.swift

--- Sources/ISSNetwork/NetworkMonitorTest.swift
+++ Sources/ISSNetwork/NetworkMonitorTest.swift
@@ -8,45 +8,70 @@
 import Combine
 import Network
 
-public final class NetworkMonitorTest: ObservableObject {
-    private var monitor: NWPathMonitor?
-    private var queue: DispatchQueue
-    
+//public final class NetworkMonitorTest: ObservableObject {
+//    private var monitor: NWPathMonitor?
+//    private var queue: DispatchQueue
+//
+//    @Published var isInternetAvailable = false
+//
+//    public var internetStatus: Bool {
+//        isInternetAvailable
+//    }
+//
+//    public init() {
+//        if monitor == nil {
+//            monitor = NWPathMonitor()
+//        }
+//        queue = DispatchQueue(label: "NetworkMonitorTest")
+//        monitor?.start(queue: queue)
+//        monitor?.pathUpdateHandler = { path in
+//            DispatchQueue.main.async {
+//                self.isInternetAvailable = path.status == .satisfied
+//            }
+//        }
+//    }
+//
+//    public func retryConnection() {
+//        if monitor == nil {
+//            monitor = NWPathMonitor()
+//        }
+//        queue = DispatchQueue(label: "NetworkMonitorTest")
+//        monitor?.start(queue: queue)
+//        monitor?.pathUpdateHandler = { path in
+//            DispatchQueue.main.async {
+//                self.isInternetAvailable = path.status == .satisfied
+//            }
+//        }
+//    }
+//
+//    public func stopMonitoring() {
+//        monitor?.cancel()
+//    }
+//}
+
+final class NetworkMonitorTest: ObservableObject {
+    @Published private(set) var isConnected = false
+    @Published private(set) var isCellular = false
     @Published var isInternetAvailable = false
 
     public var internetStatus: Bool {
-        isInternetAvailable
+        isConnected
     }
 
-    public init() {
-        if monitor == nil {
-            monitor = NWPathMonitor()
-        }
-        queue = DispatchQueue(label: "NetworkMonitorTest")
-        monitor?.start(queue: queue)
-        monitor?.pathUpdateHandler = { path in
+    private let nwMonitor = NWPathMonitor()
+    private let workerQueue = DispatchQueue.global()
+    
+    public func start() {
+        nwMonitor.start(queue: workerQueue)
+        nwMonitor.pathUpdateHandler = { [weak self] path in
             DispatchQueue.main.async {
-                self.isInternetAvailable = path.status == .satisfied
+                self?.isConnected = path.status == .satisfied
+                self?.isCellular = path.usesInterfaceType(.cellular)
             }
         }
     }
-
-    public func retryConnection() {
-        if monitor == nil {
-            monitor = NWPathMonitor()
-        }
-        queue = DispatchQueue(label: "NetworkMonitorTest")
-        monitor?.start(queue: queue)
-        monitor?.pathUpdateHandler = { path in
-            DispatchQueue.main.async {
-                self.isInternetAvailable = path.status == .satisfied
-            }
-        }
-    }
-
-    public func stopMonitoring() {
-        monitor?.cancel()
+    
+    public func stop() {
+        nwMonitor.cancel()
     }
 }
-
-
