5b0de9a189d38704bbb4f87e85236e844d807bbd
Wing Seng <chewwingseng@gmail.com>
2023-10-10 14:07:54 +0800

    Update NetworkManager.swift

--- Sources/ISSNetwork/NetworkManager.swift
+++ Sources/ISSNetwork/NetworkManager.swift
@@ -52,16 +52,17 @@ public class NetworkManager: Requestable {
                 }
 
                 // Convert the data to a JSON string for printing
-                if let jsonData = String(data: output.data, encoding: .utf8),
-                   let response = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
-                    print("JSON Response:\n\(jsonData)")
-
-                    if let resultCode = response["resultCode"] as? Int,
-                       let resultMessage = response["resultMessage"] as? String {
-                        if resultCode == 1 {
-                            throw APIError.serverError(code: response.resultCode, error: response.resultMessage)
-                        } else {
-                            print("ResultCode is not 1.")
+                if let jsonData = String(data: output.data, encoding: .utf8) {
+                    if let response = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
+                        print("JSON Response:\n\(jsonData)")
+                        
+                        if let resultCode = response["resultCode"] as? Int,
+                           let resultMessage = response["resultMessage"] as? String {
+                            if resultCode == 1 {
+                                throw APIError.serverError(code: response.resultCode, error: response.resultMessage)
+                            } else {
+                                print("ResultCode is not 1.")
+                            }
                         }
                     }
                 } else {
