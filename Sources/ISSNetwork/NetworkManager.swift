ccf43af6135c2b80b7c4f3581380eb48edd85f2f
Wing Seng <chewwingseng@gmail.com>
2023-10-17 16:40:47 +0800

    Update NetworkManager.swift

--- Sources/ISSNetwork/NetworkManager.swift
+++ Sources/ISSNetwork/NetworkManager.swift
@@ -64,7 +64,7 @@ public class NetworkManager: Requestable {
                 // throw an error if response is nil
                 guard let response = output.response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
                     let code = (output.response as? HTTPURLResponse)?.statusCode ?? 0
-                    print("Error ::: \(code)")
+                    print("Error ::: \(code), Response ::: \(response)")
                     throw APIError.serverError(code: code, error: "Something went wrong, please try again later.")
                 }
 
