0d78e64318b77cf21661932adf36546829e91dbb
Wing Seng <chewwingseng@gmail.com>
2023-10-17 16:17:55 +0800

    Update NetworkManager.swift

--- Sources/ISSNetwork/NetworkManager.swift
+++ Sources/ISSNetwork/NetworkManager.swift
@@ -42,6 +42,7 @@ public class NetworkManager: Requestable {
     }
 
     func fetchURLResponse<T>(urlRequest: URLRequest) -> AnyPublisher<T, APIError> where T: Decodable, T: Encodable {
+        print("Request ::: \(urlRequest)")
         return URLSession.shared
             .dataTaskPublisher(for: urlRequest)
             .tryMap { output in
@@ -63,6 +64,7 @@ public class NetworkManager: Requestable {
                 // throw an error if response is nil
                 guard let response = output.response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
                     let code = (output.response as? HTTPURLResponse)?.statusCode ?? 0
+                    print("Error ::: \(code)")
                     throw APIError.serverError(code: code, error: "Something went wrong, please try again later.")
                 }
 
