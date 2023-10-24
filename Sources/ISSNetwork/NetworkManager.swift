611c3f3837e34cc5c474d550006f7e9481d56c8d
Wing Seng <chewwingseng@gmail.com>
2023-10-24 11:05:05 +0800

    Update NetworkManager.swift

--- Sources/ISSNetwork/NetworkManager.swift
+++ Sources/ISSNetwork/NetworkManager.swift
@@ -47,25 +47,46 @@ public class NetworkManager: Requestable {
         return URLSession.shared
             .dataTaskPublisher(for: urlRequest)
             .tryMap { output in
+//                if let response = output.response as? HTTPURLResponse, response.statusCode == 401 {
+//                     // Use flatMap to handle token refresh asynchronously
+//                    print("Token Expired ::: \(response.statusCode)")
+//                    self.fetchRefreshTokenRequest()
+//                        .mapError { error in
+//                            // Transform the error to APIError.refreshTokenError here.
+//                            throw APIError.refreshTokenError("APIError.refreshTokenError")
+//                        }
+//                        .sink(receiveCompletion: { completion in
+//                            if case .failure(let error) = completion {
+//                                print("Refresh Token Failure")
+//                            }
+//                        }, receiveValue: { response in
+//                            print("Refresh Token Success\(response.data.appToken)")
+//                        })
+//                        .store(in: &self.cancellables)
+//
+////                    throw APIError.authenticationError(code: response.statusCode, error: "authenticationError")
+//                 }
                 if let response = output.response as? HTTPURLResponse, response.statusCode == 401 {
-                     // Use flatMap to handle token refresh asynchronously
+                    // Use flatMap to handle token refresh asynchronously
                     print("Token Expired ::: \(response.statusCode)")
-                    self.fetchRefreshTokenRequest()
-                        .mapError { error in
-                            // Transform the error to APIError.refreshTokenError here.
+                    return Just(()).tryMap { _ in
+                        try self.fetchRefreshTokenRequest()
+                    }
+                    .flatMap { result in
+                        switch result {
+                        case .success(let response):
+                            print("Refresh Token Success \(response.data.appToken)")
+                            // Handle the token refresh success, and then you can retry the original request.
+                            // Create a new request with the updated access token, and return it.
+//                            var requestWithNewAccessToken = urlRequest
+//                            requestWithNewAccessToken.addValue(response.data.appToken, forHTTPHeaderField: "Authorization")
+//                            return requestWithNewAccessToken
+                        case .failure(let error):
+                            print("Refresh Token Failure: \(error)")
                             throw APIError.refreshTokenError("APIError.refreshTokenError")
                         }
-                        .sink(receiveCompletion: { completion in
-                            if case .failure(let error) = completion {
-                                print("Refresh Token Failure")
-                            }
-                        }, receiveValue: { response in
-                            print("Refresh Token Success\(response.data.appToken)")
-                        })
-                        .store(in: &self.cancellables)
-
-//                    throw APIError.authenticationError(code: response.statusCode, error: "authenticationError")
-                 }
+                    }
+                }
                 // throw an error if response is nil
                 guard let response = output.response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
                     let code = (output.response as? HTTPURLResponse)?.statusCode ?? 0
