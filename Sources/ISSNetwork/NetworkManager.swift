39996a1b329f2ff25704239467a82ac9ec205319
Wing Seng <chewwingseng@gmail.com>
2023-10-25 12:14:52 +0800

    Update NetworkManager.swift

--- Sources/ISSNetwork/NetworkManager.swift
+++ Sources/ISSNetwork/NetworkManager.swift
@@ -360,9 +360,6 @@ public class NetworkManager: Requestable {
             .catch { error -> AnyPublisher<T, APIError> in
                 if case APIError.authenticationError = error {
                     return refreshToken()
-                        .tryMap { response in
-                            print(response.data.token.appToken)
-                        }
                         .flatMap { _ in
                             self.fetchURLResponse(urlRequest: self.updatedURLRequest, refreshToken: refreshToken)
                         }
