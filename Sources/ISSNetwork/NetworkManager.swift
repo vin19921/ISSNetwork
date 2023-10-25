f6ed25d1ceeeb8d05aebcf5927912e5026942c85
Wing Seng <chewwingseng@gmail.com>
2023-10-25 11:07:56 +0800

    Update NetworkManager.swift

--- Sources/ISSNetwork/NetworkManager.swift
+++ Sources/ISSNetwork/NetworkManager.swift
@@ -386,17 +386,17 @@ public class NetworkManager: Requestable {
                 print(output.data)
                 return output.data
             }
-            .tryMap { data in
-                guard let response = data.response as? HTTPURLResponse else {
-                    throw APIError.invalidJSON("Invalid response")
-                }
-
-                if response.statusCode == 401 {
-                    throw APIError.invalidJSON("Unauthorized request")
-                }
-
-                return data.data
-            }
+//            .tryMap { data in
+//                guard let response = data.response as? HTTPURLResponse else {
+//                    throw APIError.invalidJSON("Invalid response")
+//                }
+//
+//                if response.statusCode == 401 {
+//                    throw APIError.invalidJSON("Unauthorized request")
+//                }
+//
+//                return data.data
+//            }
             .decode(type: RefreshTokenResponse.self, decoder: JSONDecoder())
             .mapError { error in
                 if let apiError = error as? APIError {
